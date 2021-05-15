6d07da42976d059738def7882a2da7242ab920b6
altegani mohammed <tiganimoh@gmail.com>
2021-05-15 16:57:19 +0200

    refactor network functions

--- app/src/main/java/com/atechytask/twiiterclone/tweets/TweetsViewModel.kt
+++ app/src/main/java/com/atechytask/twiiterclone/tweets/TweetsViewModel.kt
@@ -1,10 +1,9 @@
 package com.atechytask.twiiterclone.tweets
 
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.atechytask.twiiterclone.data.DataOrException2
+import com.atechytask.twiiterclone.data.SignUp
 import com.atechytask.twiiterclone.data.Tweets
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -16,34 +15,14 @@ import javax.inject.Inject
 class TweetsViewModel @Inject constructor(
     private val repository: TweetsRepository
 ): ViewModel() {
-    var navigateToRegisterationPage = mutableStateOf(false)
-    var navigateToTweetsPage    = mutableStateOf(false)
-    val postTweet = mutableStateOf(false)
-
 
     @ExperimentalCoroutinesApi
     fun getTweets() = repository.getTweetsFromFirestore()
 
     fun addReply(tweets: Tweets) = repository.addTweetPost(tweets)
 
+    fun signUp(signUp: SignUp) = repository.signUpUser(signUp)
 
-    fun signUpUser(name:String,email:String,password:String,confirmPassword:String){
-        viewModelScope.launch {
-            navigateToRegisterationPage.value =
-                repository.signUpUser(name,email,password,confirmPassword)
-        }
-    }
-
-    fun signInUser(userEmail: String,userPassword:String){
-        viewModelScope.launch {
-            navigateToTweetsPage.value = repository.signInUser(userEmail,userPassword)
-        }
-    }
-
-//     fun addTweetReply(name:String, tweets:String, image:String) {
-//        viewModelScope.launch {
-//            postTweet.value = repository.addTweets(name,tweets,image)
-//        }
-//    }
+    fun signIn(userEmail: String,userPassword:String) = repository.signInUser(userEmail,userPassword)
 
 }
